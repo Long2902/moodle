eb08b8f525bcbffba0db83b1291111661587818b
Đỗ Duy Long <84571199+Long2902@users.noreply.github.com>
2026-09-07 14:43:09 +0700

    fix: bump Tiny RC1 hotfix version

--- public/lib/editor/tiny/plugins/digieramedia/version.php
+++ public/lib/editor/tiny/plugins/digieramedia/version.php
@@ -3,10 +3,10 @@
 defined('MOODLE_INTERNAL') || die();
 
 $plugin->component = 'tiny_digieramedia';
-$plugin->version = 2026090701;
+$plugin->version = 2026090702;
 $plugin->requires = 2025100600;
 $plugin->maturity = MATURITY_RC;
-$plugin->release = '1.0.0-rc1-fasttrack';
+$plugin->release = '1.0.0-rc1-fasttrack-hotfix1';
 $plugin->dependencies = [
     'local_digieramedia' => 2026090701,
 ];
