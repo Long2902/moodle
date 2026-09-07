29252919624afa3ec856bfb5e0bacb931fd4149c
Đỗ Duy Long <84571199+Long2902@users.noreply.github.com>
2026-09-07 15:54:54 +0700

    build: bump Tiny DIGIERA modal RC revision

--- public/lib/editor/tiny/plugins/digieramedia/version.php
+++ public/lib/editor/tiny/plugins/digieramedia/version.php
@@ -3,10 +3,10 @@
 defined('MOODLE_INTERNAL') || die();
 
 $plugin->component = 'tiny_digieramedia';
-$plugin->version = 2026090702;
+$plugin->version = 2026090703;
 $plugin->requires = 2025100600;
 $plugin->maturity = MATURITY_RC;
-$plugin->release = '1.0.0-rc1-fasttrack-hotfix1';
+$plugin->release = '1.0.0-rc1-fasttrack-modal-a';
 $plugin->dependencies = [
     'local_digieramedia' => 2026090701,
 ];
