6d99077c08291f09790dcee416418b5e4b209c37
Đỗ Duy Long <84571199+Long2902@users.noreply.github.com>
2026-09-08 10:59:39 +0700

    release: bump Tiny media for live R2 upload

--- public/lib/editor/tiny/plugins/digieramedia/version.php
+++ public/lib/editor/tiny/plugins/digieramedia/version.php
@@ -3,10 +3,10 @@
 defined('MOODLE_INTERNAL') || die();
 
 $plugin->component = 'tiny_digieramedia';
-$plugin->version = 2026090802;
+$plugin->version = 2026090803;
 $plugin->requires = 2025100600;
 $plugin->maturity = MATURITY_RC;
-$plugin->release = '1.0.0-rc1-fasttrack-rehydrate-metadata';
+$plugin->release = '1.0.0-rc1-fasttrack-r2-singleput';
 $plugin->dependencies = [
-    'local_digieramedia' => 2026090802,
+    'local_digieramedia' => 2026090803,
 ];
