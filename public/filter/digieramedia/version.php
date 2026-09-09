ece1d140cab3e29a39a4c48d851dd33b818132b9
Đỗ Duy Long <84571199+Long2902@users.noreply.github.com>
2026-09-09 17:16:26 +0700

    chore: bump lifecycle renderer version

--- public/filter/digieramedia/version.php
+++ public/filter/digieramedia/version.php
@@ -3,10 +3,10 @@
 defined('MOODLE_INTERNAL') || die();
 
 $plugin->component = 'filter_digieramedia';
-$plugin->version = 2026090701;
+$plugin->version = 2026090902;
 $plugin->requires = 2025100600;
 $plugin->maturity = MATURITY_RC;
-$plugin->release = '1.0.0-rc1-fasttrack';
+$plugin->release = '1.0.0-rc1-fasttrack-lifecycle';
 $plugin->dependencies = [
-    'local_digieramedia' => 2026090701,
+    'local_digieramedia' => 2026090902,
 ];
