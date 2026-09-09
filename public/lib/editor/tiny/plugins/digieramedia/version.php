1276d44950537f53f63c4a40c6be82d96c0d3cdf
Đỗ Duy Long <84571199+Long2902@users.noreply.github.com>
2026-09-09 17:12:37 +0700

    chore: bump tiny media lifecycle version

--- public/lib/editor/tiny/plugins/digieramedia/version.php
+++ public/lib/editor/tiny/plugins/digieramedia/version.php
@@ -3,10 +3,10 @@
 defined('MOODLE_INTERNAL') || die();
 
 $plugin->component = 'tiny_digieramedia';
-$plugin->version = 2026090901;
+$plugin->version = 2026090902;
 $plugin->requires = 2025100600;
 $plugin->maturity = MATURITY_RC;
-$plugin->release = '1.0.0-rc1-fasttrack-versioning2';
+$plugin->release = '1.0.0-rc1-fasttrack-lifecycle';
 $plugin->dependencies = [
-    'local_digieramedia' => 2026090901,
+    'local_digieramedia' => 2026090902,
 ];
