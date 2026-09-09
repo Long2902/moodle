4559b1ca1e0a4af87cb706cfa9d79b8ea79877eb
Đỗ Duy Long <84571199+Long2902@users.noreply.github.com>
2026-09-09 17:12:17 +0700

    chore: bump local media lifecycle version

--- public/local/digieramedia/version.php
+++ public/local/digieramedia/version.php
@@ -3,7 +3,7 @@
 defined('MOODLE_INTERNAL') || die();
 
 $plugin->component = 'local_digieramedia';
-$plugin->version = 2026090901;
+$plugin->version = 2026090902;
 $plugin->requires = 2025100600;
 $plugin->maturity = MATURITY_RC;
-$plugin->release = '1.0.0-rc1-fasttrack-versioning2';
+$plugin->release = '1.0.0-rc1-fasttrack-lifecycle';
