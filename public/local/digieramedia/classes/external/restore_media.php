9f93601e176d99e5cb61d46c2ea93ff3f86476f4
Đỗ Duy Long <84571199+Long2902@users.noreply.github.com>
2026-09-09 17:05:39 +0700

    feat: add restore media ajax endpoint

---
+++ public/local/digieramedia/classes/external/restore_media.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace local_digieramedia\external;
+
+use context;
+use core_external\external_api;
+use core_external\external_function_parameters;
+use core_external\external_single_structure;
+use core_external\external_value;
+use local_digieramedia\service\lifecycle_service;
+
+final class restore_media extends external_api {
+    public static function execute_parameters(): external_function_parameters {
+        return new external_function_parameters([
+            'contextid' => new external_value(PARAM_INT, 'Editor context id'),
+            'mediauuid' => new external_value(PARAM_ALPHANUMEXT, 'Media UUID'),
+        ]);
+    }
+
+    public static function execute(int $contextid, string $mediauuid): array {
+        global $USER;
+        $params = self::validate_parameters(self::execute_parameters(), compact('contextid', 'mediauuid'));
+        $context = context::instance_by_id($params['contextid'], MUST_EXIST);
+        self::validate_context($context);
+        return (new lifecycle_service())->restore((int)$USER->id, $context, $params['mediauuid']);
+    }
+
+    public static function execute_returns(): external_single_structure {
+        return new external_single_structure([
+            'mediauuid' => new external_value(PARAM_ALPHANUMEXT, 'Media UUID'),
+            'status' => new external_value(PARAM_ALPHA, 'Media lifecycle status'),
+            'visibility' => new external_value(PARAM_ALPHA, 'Media visibility'),
+            'livecount' => new external_value(PARAM_INT, 'Live reference count'),
+            'unresolvedreferences' => new external_value(PARAM_INT, 'References changed to UNRESOLVED'),
+            'deletedversions' => new external_value(PARAM_INT, 'Physically deleted version count'),
+        ]);
+    }
+}
