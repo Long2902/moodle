88769aada10b06c3f2305aa94bc7c99adb015c53
Đỗ Duy Long <84571199+Long2902@users.noreply.github.com>
2026-09-09 13:28:28 +0700

    chore: bump TinyMCE reference state version

--- public/lib/editor/tiny/plugins/digieramedia/version.php
+++ public/lib/editor/tiny/plugins/digieramedia/version.php
@@ -3,10 +3,10 @@
 defined('MOODLE_INTERNAL') || die();
 
 $plugin->component = 'tiny_digieramedia';
-$plugin->version = 2026090804;
+$plugin->version = 2026090901;
 $plugin->requires = 2025100600;
 $plugin->maturity = MATURITY_RC;
-$plugin->release = '1.0.0-rc1-fasttrack-versioning1';
+$plugin->release = '1.0.0-rc1-fasttrack-versioning2';
 $plugin->dependencies = [
-    'local_digieramedia' => 2026090804,
+    'local_digieramedia' => 2026090901,
 ];
